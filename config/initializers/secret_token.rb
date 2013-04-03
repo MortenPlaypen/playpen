7e9a14eb92c967750141a7d3f005f6182c1be61f
Diyah <me@hadiyah.me>
2013-04-03 16:12:18 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playpenlabs::Application.config.secret_token = '21524f8df1cdc2651db5700b4a860b6c6167fc6c84e0772560f02dbc797d770f44bc0c83a7265bfec48bf25fd4aab9bfe584df19a7650d0d7617a736ed0efb35'
